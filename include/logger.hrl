a5c487f6a4f3fa44f6f4ec8e47d5db0d3320c4a5
francescorestelli <francesco.restelli@roc-connect.com>
2020-06-21 02:59:02 +0200

    fix log

--- include/logger.hrl
+++ include/logger.hrl
@@ -2,12 +2,12 @@
 
 -define(PRINT(Format, Args), io:format(Format, Args)).
 
--define(DEBUG(Format, Args), logger:log(debug,  Format, Args, self())).
+-define(DEBUG(Format, Args), logger:log(debug,  Format, Args)).
 
--define(INFO_MSG(Format, Args), logger:log(info, Format, Args, self())).
+-define(INFO_MSG(Format, Args), logger:log(info, Format, Args)).
 
--define(WARNING_MSG(Format, Args), logger:log(warning, Format, Args, self())).
+-define(WARNING_MSG(Format, Args), logger:log(warning, Format, Args)).
 
--define(ERROR_MSG(Format, Args), logger:log(error, Format, Args, self())).
+-define(ERROR_MSG(Format, Args), logger:log(error, Format, Args)).
 
--define(CRITICAL_MSG(Format, Args), logger:log(critical, Format, Args, self())).
+-define(CRITICAL_MSG(Format, Args), logger:log(critical, Format, Args)).
